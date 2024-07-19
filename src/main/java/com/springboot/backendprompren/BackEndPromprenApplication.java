d7fa510f0ac810c0eda021b1cc9c445d424b4430
kyungbinYoo <ykb0314y@naver.com>
2024-07-19 23:25:53 +0900

    Initial commit

---
+++ src/main/java/com/springboot/backendprompren/BackEndPromprenApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.backendprompren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPromprenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndPromprenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/backendprompren/BackEndPromprenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.backendprompren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPromprenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
