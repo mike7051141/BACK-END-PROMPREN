be764ab36a084bdd02418017b5343c8f7fedf98f
mike7051141 <mike705114@gmail.com>
2024-07-23 20:53:30 +0900

    Refactor competition
    - HttpStatus 추가
    - title 중복 체크 로직 추가

--- src/main/java/com/springboot/backendprompren/data/repository/CompetitionRepository.java
+++ src/main/java/com/springboot/backendprompren/data/repository/CompetitionRepository.java
@@ -3,5 +3,8 @@ package com.springboot.backendprompren.data.repository;
 import com.springboot.backendprompren.data.entity.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
+    Optional<Competition> findByTitle(String title);
 }
